2f161741481b76a539bff159500f7b376e3f09ad
Bill Allen <BillAllenEmail@gmail.com>
2019-03-06 15:43:08 -0600

    MOD working on jenkins build

--- src/test/java/com/agileinnov/dojo/RomanNumeralTest.java
+++ src/test/java/com/agileinnov/dojo/RomanNumeralTest.java
@@ -6,8 +6,7 @@ import org.junit.Test;
 public class RomanNumeralTest {
 
     @Test
-    public void returnsIwhenGiven1() {
-        Assert.assertEquals("I", com.agileinnov.dojo.RomanNumeral.of(1));
+    public void returnsIwhenGiven1() { Assert.assertEquals("I", com.agileinnov.dojo.RomanNumeral.of(1));
     }
 
     @Test
